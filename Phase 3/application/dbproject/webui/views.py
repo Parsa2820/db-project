381a275fc714af0c2bb134737673b09a04ff0ce8
Parsa Mohammadian <parsa2820@gmail.com>
2022-02-06 17:14:09 +0330

    Get data by query
    Co-authored-by: Arshan Dalili <arshandalili@users.noreply.github.com>

--- Phase 3/application/dbproject/webui/views.py
+++ Phase 3/application/dbproject/webui/views.py
@@ -2,14 +2,19 @@ from shutil import ExecError
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db import connection
+from matplotlib.pyplot import table
 from matplotlib.style import context
+from django.core.exceptions import FieldError
 from django.db.models import Field
 from django.apps import apps
 
 
-TABLES = [x.__name__ for x in apps.get_models() if not x.__name__.startswith('Auth') and not x.__name__.startswith('Django')]
-REQUEST_FIELD_KEY = 'field'
-REQUEST_VALUE_KEY = 'field-value'
+TABLES = [x.__name__ for x in apps.get_models() if not x.__name__.startswith(
+    'Auth') and not x.__name__.startswith('Django')]
+REQUEST_TABLE_KEY = 'table'
+REQUEST_FIELD_KEY = 'column'
+REQUEST_VALUE_KEY = 'value'
+
 
 def home(request):
     context = {
@@ -18,11 +23,21 @@ def home(request):
         'data': []
     }
     if request.method == 'POST':
-        table_name = request.POST.get('table', None)
-        model = apps.get_model('webui', table_name)
-        context['header'] = [x.name for x in model._meta.get_fields() if isinstance(x, Field)]
-        context['data'] = [x.__dict__ for x in model.objects.all()]
+        table_name = request.POST.get(REQUEST_TABLE_KEY, None)
+        field_name = request.POST.get(REQUEST_FIELD_KEY, None)
+        field_value = request.POST.get(REQUEST_VALUE_KEY, None) 
+        if table_name:
+            model = apps.get_model('webui', table_name)
+            context['header'] = [x.name for x in model._meta.get_fields() if isinstance(x, Field)]
+        if table_name and field_name and field_value:
+            try:
+                context['data'] = [x.__dict__ for x in model.objects.filter(**{field_name: field_value})]
+            except FieldError:
+                pass
+        elif table_name:
+            context['data'] = [x.__dict__ for x in model.objects.all()]
     return render(request, 'webui/home.html', context)
+
 
 def about(request):
     return render(request, 'webui/about.html')
