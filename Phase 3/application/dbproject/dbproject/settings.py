8cdcf1ea17f4cf5eb036a027312521684b9f0d1b
Parsa Mohammadian <parsa2820@gmail.com>
2022-02-06 15:04:06 +0330

    Connect to database and add models
    Co-authored-by: Arshan Dalili <arshandalili@users.noreply.github.com>

--- Phase 3/application/dbproject/dbproject/settings.py
+++ Phase 3/application/dbproject/dbproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'dbproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbprojectdev',
+        'USER': 'postgres',
+        'PASSWORD': 'asdf',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- Phase 3/application/dbproject/webui/views.py
+++ Phase 3/application/dbproject/webui/views.py
@@ -1,10 +1,24 @@
+from shutil import ExecError
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
 from matplotlib.style import context
+
+
 
 def home(request):
     context = {
+        'tables': connection.introspection.table_names(),
+        'header': [],
+        'data': []
     }
+    if request.method == 'POST':
+        search_id = request.POST.get('textfield', None)
+        try:
+            user = str(search_id)
+            # fill context
+        except Exception:
+            return HttpResponse("Not a query!")
     return render(request, 'webui/home.html', context)
 
 def about(request):
