539240e2651d9667d97bb39bf85a0d51fe9c9e46
saaz742 <azarnooshsa@gmail.com>
2022-02-07 22:45:01 +0330

    more data

--- Phase 3/database-query/data.sql
+++ Phase 3/database-query/data.sql
@@ -19,7 +19,7 @@ VALUES(
     'abi',
     'brim',
     '1/1/2001',
-    '09112223345',
+    '0911222335',
     '0216666666',
     '1231231231',
     'france',
@@ -50,7 +50,7 @@ VALUES(
     'abi',
     'brim',
     '1/1/2001',
-    '09112223345',
+    '0911222334',
     '0216666666',
     'Employee',
     '1111111',
@@ -71,8 +71,8 @@ insert into EmployeeSchedule (
 VALUES(
     '1111111112',
     'Saturday',
-    '1/1/2020',
-   
+    '8:00',
+    '16:00'
 );
 
 insert into Account(
@@ -86,6 +86,9 @@ insert into Account(
 VALUES(
     '1111111112',
     'azra',
+    'azra@gmail.com',
+    'f0e4c2f76c58916ec258f246851bea3b',
+    '0912222222'
 );
 
 insert into BankAccount(
@@ -261,4 +264,271 @@ VALUES(
     121212,
     'azra',
     '1111111112'
+);
+
+insert into  RealPerson(
+    nationalId,
+    firstName,
+    lastName,
+    fatherName,
+    birthDate,
+    mobileNumber,
+    landlineNumber,
+    postalCode,
+    country,
+    city,
+    addresExtra
+)
+
+VALUES(
+    '1111111444',
+    'angela',
+    'pitt',
+    'brad',
+    '1/2/2001',
+    '0911222444',
+    '0216666444',
+    '1231231444',
+    'france',
+    'paris',
+    'route street'
+);
+
+insert into Employee(
+    nationalId,
+    firstName,
+    lastName,
+    fatherName,
+    birthDate,
+    mobileNumber,
+    landlineNumber,
+    _role,
+    monthlySalary,
+    startDate,
+    postalCode,
+    country,
+    city,
+    addresExtra
+)
+
+VALUES(
+    '1111111444',
+    'angela',
+    'pitt',
+    'brad',
+    '1/2/2001',
+    '0911222444',
+    '0216666444',
+    'Employee',
+    '11111444',
+    '1/2/2020',
+    '1231231444',
+    'france',
+    'paris',
+    'route street'
+);
+
+insert into EmployeeSchedule (
+    emplyeeNationalId,
+    _weekDay,
+    startHour,
+    endHour
+)
+
+VALUES(
+    '1111111444',
+    'Saturday',
+    '8:00',
+    '16:00'
+);
+
+insert into Account(
+    realPersonNationalId,
+    username,
+    email,
+    accountPassword,
+    phoneNumber
+)
+
+VALUES(
+    '1111111444',
+    'angela',
+    'angela@gmail.com',
+    'f0e4c2f76c58916ec258f246851be444',
+    '0912222444'
+);
+
+insert into BankAccount(
+    id,
+    active,
+    balance,
+    iban,
+    openDate,
+    creatorUsername
+)
+
+VALUES(
+    '121444',
+    'True',
+    '1111444',
+    '1111111111111111111444',
+    '1/4/2020',
+    'angela'
+);
+
+insert into QarzolHasana(
+    id
+)
+
+VALUES(
+    1212444
+);
+
+insert into Saving(
+    id,
+    profit
+)
+
+VALUES(
+    1212444,
+    0.1
+);
+
+insert into _Card(
+    cardNumber,
+    primaryPassword,
+    secondaryPassword,
+    expirationDate,
+    CVV1,
+    CVV2,
+    active,
+    bankAccount
+)
+
+VALUES(
+    '111111111111111444',
+    '1444',
+    '11111444',
+    '1/1/2222',
+    '1444',
+    '1444',
+    'True',
+    121444
+);
+
+insert into _Transaction(
+    transactionId,
+    source,
+    destination,
+    _date,
+    _description,
+    amount,
+    trackingId
+)
+
+VALUES(
+    '1212444',
+    111111111111111111,
+    111111111111111444,
+    '1/4/2020',
+    'store',
+    '11144',
+    '123123444'
+);
+
+insert into Purchase(
+    transactionId,
+    storeName
+)
+
+VALUES(
+    1212444,
+    'digikala'
+);
+
+insert into PayBill(
+    transactionId,
+    billId,
+    paymentId
+)
+
+VALUES(
+    1212444,
+    '1111111111444',
+    '111111111444'
+);
+
+insert into WireTransfer(
+    transactionId
+)
+
+VALUES(
+    1212444
+);
+
+insert into Deposit(
+    transactionId
+)
+
+VALUES(
+    1212444
+);
+
+insert into Withdraw(
+    transactionId
+)
+
+VALUES(
+    1212444
+);
+
+insert into UserRequest(
+    requestId,
+    _status,
+    _date,
+    response,
+    _description
+)
+
+VALUES(
+    121444,
+    'Done',
+    '1/4/2020',
+    'accepted',
+    'ok'
+);
+
+
+insert into CreateBankAccountRequest(
+    requestId,
+    username,
+    bankAccountType
+)
+
+VALUES(
+    121444,
+    'angela',
+    'QarzolHasana'
+);
+
+insert into CreateCardRequest(
+    requestId,
+    bankAccountId
+)
+
+VALUES(
+    121444,
+    121444
+);
+
+insert into Support(
+    requestId,
+    username,
+    employeeNationId
+)
+
+VALUES(
+    121444,
+    'angela',
+    '1111111444'
 );
